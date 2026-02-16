09719a6ec03ecb59daa087d3b2ba59329c6265db
pdmn <padmini.velidi@gmail.com>
2026-02-16 12:36:34 +0000

    Next Update project files with MLOps pipeline and Streamlit app

---
+++ hosting/hosting.py
@@ -0,0 +1,13 @@
+
+from huggingface_hub import HfApi
+import os
+
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="tourism_project/deployment",     # the local folder containing your files
+    repo_id="padminiv26/MLOps_topurism",          # the target repo
+    repo_type="space",                      # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+)
